a461cc35035c8baf1a8ecd9d6a64ccf285283dff
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-08-15 22:27:09 +0430

    Array.from write array by length and index

--- vanilaJS/script.js
+++ vanilaJS/script.js
@@ -684,6 +684,10 @@ document.querySelector('.r37-6').innerHTML =range(0, 400, 50);
 document.querySelector('.r37-7').innerHTML =range('A'.charCodeAt(0), 'Z'.charCodeAt(0), 1);
 document.querySelector('.r37-8').innerHTML =range('A'.charCodeAt(0), 'Z'.charCodeAt(0), 1).map(x => String.fromCharCode(x));
 
+document.querySelector('.r37-9').innerHTML = Array.from({ length:5 } , (_, index) =>{
+    return index * 2
+})
+
 
 /*********************/
 
